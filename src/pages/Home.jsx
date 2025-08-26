86402b53fbfc85203ba7d43e6dc1047e74a67cd7
Mark Aine <ainemarkjr@gmail.com>
2025-08-26 20:18:46 +0300

    Made the Search Movies engine functional

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,9 +25,24 @@ function Homepage() {
     loadPopularMovies();
   }, []);
 
- function handleSearch(e){ 
+ const handleSearch = async(e) =>{ 
   e.preventDefault();
-   alert(searchQuery); 
+   if(!searchQuery.trim()) return
+   if (loading) return
+
+  setLoading(true)
+  try{
+     const searchResults = await searchMovies(searchQuery)
+     setMovies(searchResults)
+     setError(null)
+
+  } catch (err){
+    console.log(err)
+    setError("Failed to search movies...")
+
+  } finally{
+    setLoading(false)
+  }
    setSearchQuery("") 
   }
 
